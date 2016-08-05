a191eec6043ad2ad5fddd4cade78256838018188
Pawel Jakubowski <p.m.jakubowski@gmail.com>
2016-08-05 21:53:58 +0200

    feat(funga): remove `consume` and `resolve` methods, rename `emit` as `factory`

--- src/funga.js
+++ src/funga.js
@@ -1,25 +1,13 @@
 'use strict';
 
 var Emitter = require('./Emitter');
-var ConsumingEmitter = require('./ConsumingEmitter');
-var ResolvingEmitter = require('./ResolvingEmitter');
+
+function factory(base) {
+    return function() {
+        return new Emitter(base, Array.prototype.slice.call(arguments));
+    };
+}
 
 module.exports = {
-    emit: function(base) {
-        return function() {
-            return new Emitter(base, Array.prototype.slice.call(arguments));
-        };
-    },
-    consume: function(digest) {
-        return function() {
-            return new ConsumingEmitter(digest, Array.prototype.slice.call(arguments));
-        };
-    },
-    resolve: function(emitterFactory) {
-        return function() {
-            return new ResolvingEmitter(
-                emitterFactory(Array.prototype.slice.call(arguments))
-            );
-        };
-    }
+    factory: factory
 };
--- test/funga-test.js
+++ test/funga-test.js
@@ -2,57 +2,22 @@
 
 var compareEmitters = require('../src/compareEmitters');
 var Emitter = require('../src/Emitter');
-var ConsumingEmitter = require('../src/ConsumingEmitter');
-var ResolvingEmitter = require('../src/ResolvingEmitter');
 var funga = require('../src/funga');
 
 describe('funga', function() {
 
-    describe('emit', function() {
+    describe('#factory', function() {
 
-        it('should return emitter with provided base and arguments', function() {
+        it('should return emitter factory', function() {
             var base = function() {};
             var args = [{a:1}, {b:2}];
-            var emit = funga.emit(base);
-            var emitter = emit(args[0], args[1]);
+            var emitterFactory = funga.factory(base);
+            var emitter = emitterFactory(args[0], args[1]);
 
             expect(emitter instanceof Emitter).toBe(true);
             expect(emitter.base).toBe(base);
             expect(emitter.args[0]).toBe(args[0]);
             expect(emitter.args[1]).toBe(args[1]);
-        });
-
-    });
-
-    describe('consume', function() {
-
-        it('should return consuming emitter with provided digest and arguments', function() {
-            var digest = function() {};
-            var args = [new Emitter(function() {}), {}];
-            var consume = funga.consume(digest);
-            var consumingEmitter = consume(args[0], args[1]);
-
-            expect(consumingEmitter instanceof ConsumingEmitter).toBe(true);
-            expect(consumingEmitter.args[0]).toBe(digest);
-            expect(consumingEmitter.args[1]).toBe(args[0]);
-            expect(consumingEmitter.args[2]).toBe(args[1]);
-        });
-
-    });
-
-    describe('resolve', function() {
-
-        it('should create resolving emitter with emitter created by provided emitter factory with arguments', function() {
-            var args = [{}, {}];
-            var resolve = funga.resolve(function() {
-                return new Emitter(function() {}, args); 
-            });
-            var resolvingEmitter = resolve(args[0], args[1]);
-
-            expect(resolvingEmitter instanceof ResolvingEmitter).toBe(true);
-            expect(resolvingEmitter.args[0] instanceof Emitter).toBe(true);
-            expect(resolvingEmitter.args[0].args[0]).toBe(args[0]);
-            expect(resolvingEmitter.args[0].args[1]).toBe(args[1]);
         });
 
     });
