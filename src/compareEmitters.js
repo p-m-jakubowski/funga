c67b5a6726bd4d5b4d63c722cd05c5426c85bc58
Pawel Jakubowski <p.m.jakubowski@gmail.com>
2016-08-05 21:50:55 +0200

    feat(compareEmitters): change emitter's args comparing

--- src/compareEmitters.js
+++ src/compareEmitters.js
@@ -12,11 +12,37 @@ function compareEmitters(emitterA, emitterB) {
     }
 
     for (var i in emitterA.args) {
+        if (emitterA.args[i] === emitterB.args[i]) {
+            continue;
+        }
+
         if (emitterA.args[i] instanceof Emitter && emitterB.args[i] instanceof Emitter) {
             if (!compareEmitters(emitterA.args[i], emitterB.args[i])) {
                 return false;
             }
-        } else if (emitterA.args[i] !== emitterB.args[i]) {
+        } else if (typeof emitterA.args[i] === 'object' && typeof emitterB.args[i] === 'object') {
+            if (!compareObjects(emitterA.args[i], emitterB.args[i])) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function compareObjects(objectA, objectB) {
+    if (objectA === null || objectB === null) {
+        return false;
+    }
+
+    if (Object.keys(objectA).length !== Object.keys(objectB).length) {
+        return false;
+    }
+
+    for (var i in objectA) {
+        if (objectA[i] !== objectB[i]) {
             return false;
         }
     }
--- test/compareEmitters-test.js
+++ test/compareEmitters-test.js
@@ -14,35 +14,38 @@ describe('compareEmitters', function() {
 
     it('should return false for emitters with different arguments length', function() {
         var base = function() {};
-        var emitterA = new Emitter(base, [1,2]);
-        var emitterB = new Emitter(base, [1,2,3]);
+        var emitterA = new Emitter(base, [1, 2]);
+        var emitterB = new Emitter(base, [1, 2, 3]);
 
         expect(compareEmitters(emitterA, emitterB)).toBe(false);
     });
 
     it('should return false for emitters with different non-resolvable arguments', function() {
         var base = function() {};
-        var ob = {};
-        var ob2 = {};
-        var emitterA = new Emitter(base, [1, ob]);
-        var emitterB = new Emitter(base, [1, ob2]);
+        var emitterA = new Emitter(base, [{a: 1}]);
+        var emitterB = new Emitter(base, [{a: 2}]);
+        var emitterC = new Emitter(base, [{a: 1, b: 2}]);
+        var emitterD = new Emitter(base, [null]);
+        var emitterE = new Emitter(base, [1]);
 
         expect(compareEmitters(emitterA, emitterB)).toBe(false);
+        expect(compareEmitters(emitterA, emitterC)).toBe(false);
+        expect(compareEmitters(emitterA, emitterD)).toBe(false);
+        expect(compareEmitters(emitterA, emitterE)).toBe(false);
     });
 
     it('should return false for emitters with different resolvable arguments', function() {
         var base = function() {};
-        var emitterA = new Emitter(base, [1, new Emitter(function() {})]);
-        var emitterB = new Emitter(base, [1, new Emitter(function() {})]);
+        var emitterA = new Emitter(base, [new Emitter(function() {})]);
+        var emitterB = new Emitter(base, [new Emitter(function() {})]);
 
         expect(compareEmitters(emitterA, emitterB)).toBe(false);
     });
 
     it('should return true in other case', function() {
         var base = function() {};
-        var ob = {};
-        var emitterA = new Emitter(base, [1, ob, new Emitter(base)]);
-        var emitterB = new Emitter(base, [1, ob, new Emitter(base)]);
+        var emitterA = new Emitter(base, [1, {a: 1, b: 2}, new Emitter(base)]);
+        var emitterB = new Emitter(base, [1, {a: 1, b: 2}, new Emitter(base)]);
 
         expect(compareEmitters(emitterA, emitterB)).toBe(true);
     });
